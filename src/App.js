4043cfbedacd0c958cb31d204bbd483ea6e5642e
vikhulei <vikhulei@gmail.com>
2021-06-20 22:02:25 +0300

    Started creating the form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+const [inputValue, setInputValue] = useState("")
+
+const addTodo =() => {
+  
+}
+
   return (
     <div className="App">
-
+      <div className="container">
+        <form onSubmit={addTodo}>
+          <input
+            type="text"
+            placeholder="Add task..."
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+            />
+            <button type="submit">Add</button>
+        </form>
+      </div>
     </div>
   );
 }
