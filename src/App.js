fb344d44782c983e80e701882c9d74576ab3be02
vikhulei <vikhulei@gmail.com>
2021-06-22 08:09:55 +0300

    Edit items functionality added

--- src/App.js
+++ src/App.js
@@ -8,19 +8,41 @@ const firstRender = useRef(true)
 
 const [inputValue, setInputValue] = useState("")
 const [todos, setTodos] = useState([])
+const [buttonName, setButtonName] = useState("Add")
+const [itemId, setItemId] = useState()
 
-const addTodo =(e) => {
+const addTodo = (e) => {
   e.preventDefault()
-  if (inputValue.trim() === "") return;
-  setTodos([...todos, {
+  if(buttonName==="Edit") {
+    let ls = JSON.parse(localStorage.getItem("Todo"))
+    ls.map(val => {
+      if(val.id===itemId) {
+       val.text=inputValue
+      }
+    })
+    setTodos(ls)
+    setInputValue("")
+    setButtonName("Add")
+  } else if (inputValue.trim() === "") {
+    return;
+  } else  {
+    setTodos([...todos, {
     text: inputValue,
     id: uuidv4()
   }])
   setInputValue("")
+  }
 }
 
 const removetodo = (id) => {
   setTodos(todos.filter(todo => todo.id !== id))
+}
+
+const edittodo = (id) => {
+  let result = todos.filter(todo => todo.id === id)
+  setInputValue(result.map(val => val.text))
+  setButtonName("Edit")
+  setItemId(id)
 }
 
 useEffect(() => {
@@ -49,12 +71,15 @@ useEffect(() => {
             value={inputValue}
             onChange={(e) => setInputValue(e.target.value)}
             />
-            <button type="submit">Add</button>
+            <button type="submit">{buttonName}</button>
         </form>
         {todos.map(todo => (
           <div key={todo.id} className="todo">
             <p>{todo.text}</p>
+            <div className="todoIcons">
             <i onClick={() => removetodo(todo.id)} className="fas fa-trash-alt"></i>
+            <i onClick={() => edittodo(todo.id)} className="fas fa-edit"></i>
+            </div>
           </div>
         ))}
       </div>
